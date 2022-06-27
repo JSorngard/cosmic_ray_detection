d4f2a75df4b236c31cb90be09e2b5d3899c258d7
Johanna Sörngård <johanna.sorngard@fysik.su.se>
2022-06-27 15:46:55 +0200

    Expanded help string for clap

--- src/main.rs
+++ src/main.rs
@@ -135,7 +135,7 @@ impl Config {
             .author("Johanna Sörngård (jsorngard@gmail.com)")
             .arg(
                 Arg::with_name("memory_size")
-                    .help("the size of the memory to monitor for bit flips")
+                    .help("the size of the memory to monitor for bit flips, understands e.g. 200, 5kB, 2GB and 3Mb")
                     .short('m')
                     .takes_value(true)
                     .required(true),
