a63df9cd49d63919f7bf31ec89ee9c07f598a3bf
Johanna Sörngård <johanna.sorngard@fysik.su.se>
2022-07-04 13:22:58 +0200

    Moved input argument parsing to the config module

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,7 @@ const MEMORY_DEFAULT: &str = "1GB";
 const DELAY_DEFAULT: &str = "30000";
 
 pub struct Config {
-    pub memory_to_occupy: String,
+    pub memory_to_occupy: usize,
     pub check_delay: u64,
     pub parallel: bool,
     pub verbose: bool,
@@ -52,7 +52,10 @@ impl Config {
 
         let verbose = !args.is_present("quiet");
 
-        let memory_to_occupy = args.value_of("memory_size").unwrap_or("1GB").to_owned();
+        let memory_to_occupy = match parse_size_string(args.value_of("memory_size").unwrap_or("1GB").to_owned()) {
+            Ok(s) => s,
+            Err(e) => return Err(e),
+        };
 
         let check_delay: u64 = match args.value_of("check_delay") {
             Some(s) => match s.parse() {
@@ -68,5 +71,57 @@ impl Config {
             parallel,
             verbose,
         })
+    }
+}
+
+///Parses a string describing a number of bytes into an integer.
+///The string can use common SI prefixes as well, like '4GB' or '30kB'.
+fn parse_size_string(size_string: String) -> Result<usize, String> {
+    match size_string.parse::<usize>() {
+        Ok(t) => Ok(t),
+        Err(_) => {
+            let chars: Vec<char> = size_string.chars().collect();
+            let len: usize = chars.len();
+            let last: char = match chars.last() {
+                Some(l) => *l,
+                None => return Err("memory_to_occupy was empty".to_owned()),
+            };
+
+            if (last != 'B' && last != 'b') || len < 2 {
+                return Err("memory_to_occupy was incorrectly formatted".to_owned());
+            }
+
+            let next_to_last: char = chars[len - 2];
+
+            let si_prefix_factor: f64 = if next_to_last == 'k' {
+                1e3
+            } else if next_to_last == 'M' {
+                1e6
+            } else if next_to_last == 'G' {
+                1e9
+            } else if next_to_last == 'T' {
+                //Future proofing...
+                1e12
+            } else if next_to_last == 'P' {
+                //HOW?!
+                1e15
+            } else if !next_to_last.is_digit(10) {
+                return Err("unsupported memory size".to_owned());
+            } else {
+                return Err("could not parse memory size".to_owned());
+            };
+
+            let bit_size: f64 = if last == 'B' { 1.0 } else { 1.0 / 8.0 };
+
+            let factor: usize = (si_prefix_factor * bit_size) as usize;
+
+            let digits: String = chars[..len - 2].iter().collect();
+            let number: usize = match digits.parse() {
+                Ok(n) => n,
+                Err(e) => return Err(e.to_string()),
+            };
+
+            Ok(number * factor)
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,7 @@ fn main() -> Result<(), String> {
         Err(e) => return Err(e),
     };
 
-    let size: usize = match parse_size_string(conf.memory_to_occupy) {
-        Ok(s) => s,
-        Err(e) => return Err(e),
-    };
+    let size = conf.memory_to_occupy;
 
     let verbose: bool = conf.verbose;
     let parallel: bool = conf.parallel;
@@ -135,54 +132,4 @@ fn check_detector_equals(detector: &[u8], value: u8, parallel: bool) -> bool {
 #[inline(always)]
 fn flush() {
     stdout().flush().unwrap();
-}
-
-fn parse_size_string(size_string: String) -> Result<usize, String> {
-    match size_string.parse::<usize>() {
-        Ok(t) => Ok(t),
-        Err(_) => {
-            let chars: Vec<char> = size_string.chars().collect();
-            let len: usize = chars.len();
-            let last: char = match chars.last() {
-                Some(l) => *l,
-                None => return Err("memory_to_occupy was empty".to_owned()),
-            };
-
-            if (last != 'B' && last != 'b') || len < 2 {
-                return Err("memory_to_occupy was incorrectly formatted".to_owned());
-            }
-
-            let next_to_last: char = chars[len - 2];
-
-            let si_prefix_factor: f64 = if next_to_last == 'k' {
-                1e3
-            } else if next_to_last == 'M' {
-                1e6
-            } else if next_to_last == 'G' {
-                1e9
-            } else if next_to_last == 'T' {
-                //Future proofing...
-                1e12
-            } else if next_to_last == 'P' {
-                //HOW?!
-                1e15
-            } else if !next_to_last.is_digit(10) {
-                return Err("unsupported memory size".to_owned());
-            } else {
-                return Err("could not parse memory size".to_owned());
-            };
-
-            let bit_size: f64 = if last == 'B' { 1.0 } else { 1.0 / 8.0 };
-
-            let factor: usize = (si_prefix_factor * bit_size) as usize;
-
-            let digits: String = chars[..len - 2].iter().collect();
-            let number: usize = match digits.parse() {
-                Ok(n) => n,
-                Err(e) => return Err(e.to_string()),
-            };
-
-            Ok(number * factor)
-        }
-    }
 }
